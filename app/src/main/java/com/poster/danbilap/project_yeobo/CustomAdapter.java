f3dff29d9a3488a9b751645e71bd28bf9851ecda
DAHUN\dh814_000 <dh8140@hanmail.net>
2016-11-19 02:16:19 +0900

    합치고, 로딩 고침!

--- app/src/main/java/com/poster/danbilap/project_yeobo/CustomAdapter.java
+++ app/src/main/java/com/poster/danbilap/project_yeobo/CustomAdapter.java
@@ -1,19 +1,15 @@
 package com.poster.danbilap.project_yeobo;
 
 import android.content.Context;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.ImageView;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import com.bumptech.glide.Glide;
 import com.example.danbilap.project_yeobo.R;
-import com.tsengvn.typekit.TypekitContextWrapper;
 
 import java.util.ArrayList;
 
